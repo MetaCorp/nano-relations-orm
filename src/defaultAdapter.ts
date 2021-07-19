348fe8647c15fb33648642d12864a185d7609737
Léopold Szabatura <l.szabatura@gmail.com>
2021-07-19 22:03:58 +0200

    Fix TS2322

--- src/defaultAdapter.ts
+++ src/defaultAdapter.ts
@@ -1,14 +1,14 @@
 
-const find = async <T>(entityName: string, where: any, orderBy?: any, limit?: number, offset?: number): Promise<T[]> => {
-  return
+const find = async <T>(entityName: string, where: any, orderBy?: any, limit?: number, offset?: number): Promise<T[] | null> => {
+  return null
 }
 
 const findOne = async <T>(entityName: string, where: any, options?: any | string[]): Promise<T | null> => {
-  return
+  return null
 }
 
 const nativeInsert = async (entityName: string, data: any): Promise<any> => {
-  return
+  return null
 }
 
 export default {
